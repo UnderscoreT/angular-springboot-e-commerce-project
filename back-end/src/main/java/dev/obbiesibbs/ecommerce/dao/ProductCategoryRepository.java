0f47cdbd2e49d4015fd208312f3aab83a183dc0b
OBEY <obeyblessing@gmail.com>
2024-07-18 22:59:43 +0200

    cors

--- back-end/src/main/java/dev/obbiesibbs/ecommerce/dao/ProductCategoryRepository.java
+++ back-end/src/main/java/dev/obbiesibbs/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,7 @@ package dev.obbiesibbs.ecommerce.dao;
 import dev.obbiesibbs.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel ="productCategory",//name of json entry
 path = "product-category") //path /product-category
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {

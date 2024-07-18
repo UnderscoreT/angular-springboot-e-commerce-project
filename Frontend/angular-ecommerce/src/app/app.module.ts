215059af5ef4811c8365484adef682c761d482a6
OBEY <obeyblessing@gmail.com>
2024-07-19 00:16:30 +0200

    services/comps/..

---
+++ Frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [
+    ProductService,
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

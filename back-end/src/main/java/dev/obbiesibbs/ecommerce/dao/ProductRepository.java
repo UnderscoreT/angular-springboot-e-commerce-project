8cb0f4dd0b36d3c60f599fc8fbb5cc5612a475f8
OBEY <obeyblessing@gmail.com>
2024-07-19 00:15:02 +0200

    origins

--- back-end/src/main/java/dev/obbiesibbs/ecommerce/dao/ProductRepository.java
+++ back-end/src/main/java/dev/obbiesibbs/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,6 @@ package dev.obbiesibbs.ecommerce.dao;
 
 import dev.obbiesibbs.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
